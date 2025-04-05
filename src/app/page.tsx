ab3c84d71860cddc03c24232febcb24af0e910b1
Wesley Nascimento <wnascx@gmail.com>
2025-04-05 14:49:55 -0300

    feature: add input search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="flex justify-center items-center h-screen w-full">
+      <Input type="text" placeholder="Pesquise o código do calibrador aqui" className="w-80 text-blue-800" />
     </div>
   );
 }
